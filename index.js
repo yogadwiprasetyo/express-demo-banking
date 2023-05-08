d9c47403cc3c14b566ef8d2f4526ed96f7710cf6
yogadwiprasetyo <jobsforyoga@gmail.com>
2023-05-08 09:38:07 +0700

    fix(api): success triggered transaction

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { request } = require("urllib");
 const { v4: uuidv4 } = require("uuid");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3030;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -16,8 +16,28 @@ app.post("/send-transaction", async (requestParam, responseParam) => {
       "https://sistemadev.com/asp/osp/authrequest?customer=7824",
       {
         method: "POST",
-        content: payload,
-        contentType: "json",
+        content: JSON.stringify({
+          messageId: `TRX_${uuidv4()}`,
+          messageType: "AUTH",
+          ts: "oFEEC777D7",
+          userId: null,
+          deviceId: null,
+          payloadData: {
+            passType: "2",
+            notifyMsgFlag: "0",
+            notifyMsgData: {
+              textToDisplay: "Authenticate your transaction"
+            },
+            msgFlag: "0",
+            msgData: {
+              data: "zUvcArlRssvv+LVV4LEbQ96JYlZQJlePAQeBlAvlwnEfizffh/+HCCD5P/mJrxObk7JqTCH3vKApJzt63kJ+VT/x51jcmWI8PnEETTjxDSI3+vNWuu4qHzrggu8j+vxlgUSbIPUXYd6DztxJfu/yxixIZcb7g5KGhXyWdsyrQlfcIt7PNv/K6MqB8wS3tgTJRo24yZhsyN4kBa3uh8xPd+g1jA74pI/5/KcAZBJp4BfcQxM53wVKvWWEsAhc8o/SE4vWe5rzuw2dgjQIxJbM0QCTbOsiS4mC3qKbM2JEPF85g35l65LKuaWhugRFBzRb"
+            }
+          },
+          callbackUrl: "https://sistemadev.com/osp",
+          vseSign: "1",
+          appBundleId: "id.co.sistema.vkey"
+        }),
+        headers: { 'Content-Type': 'application/json', },
         timeout: 60000,
         digestAuth: "7824:f33e1e4c6d6bd0aed9ebab909e059391",
       }
